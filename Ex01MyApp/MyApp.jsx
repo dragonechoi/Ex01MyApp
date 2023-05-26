2e8ea6d5a50af0758d22fa51b910a044d98c7117
dragonechoi <vasco1379@naver.com>
2023-05-26 10:53:10 +0900

    update) 중첩뷰 사용

--- Ex01MyApp/MyApp.jsx
+++ Ex01MyApp/MyApp.jsx
@@ -18,16 +18,55 @@ class MyApp extends Component{
 
             //뷰 그룹을 사용 (컴포넌트 그룹)
             // JSX는 xml안에서도 JS의 변수 ,함수 사용이 가능
-            //XML 안에서 {}는 JS 문법을 쓰는 영역 전체 코드처리는 할수없지만 
-            <View>
-                <Text>Hello {name} 님</Text>
-                <Text>Nice to meet you</Text>
+            //XML 안에서 {}는 JS 문법을 쓰는 영역 
+            //스타일 속성 적용 - 
+            <View style={ rootStyle }>
+                <Text style={textStyle}>Hello {name} 님</Text>
+                <Text style={plainTextStyle}>Nice to meet you</Text>
+                {/* Button Component는 style 속성이 존재하지 않음 */}
+                {/* <Button title={buttonTitle}></Button> */}
+                <View style={{marginTop:16}}>
+
                 <Button title={buttonTitle}></Button>
+
+                </View>
             </View>
+           
         )
     }//render method 
 
 } //My App Class
+
+//스타일객체 - 스타일 속성값은 css를 기반으로 제작
+const textStyle={
+    color: 'red',
+    fontSize : 20, //기본 단위는 dp
+    firntWeight: 'bold',
+    margin:16
+}
+// RN는 기본적으로 display:felx가 적용되어 있음.
+// 기본 배치 방향[flex-direction]이 column[세로] 방향
+// 기본적으로 컴포넌트의 height은 wrap 임
+const rootStyle={
+    padding:16 ,
+    backgroundColor:'#a3d15f',
+    // height: '100%'
+    // flex-grow속성 [android 의 layout-weight과 비슷한 속성]
+    //RN 에서는 flex-grow속성을 그냉 flex 로 사용
+    flex:1,
+    // flexDirection:'row'
+}
+
+//중간에 있는 글씨 스타일
+const plainTextStyle={
+    margin:8,
+    color:'black',
+}
+//버튼의 스타일 객체 는 무의미함 . 버튼 컴포넌트는 스타일링 불가능 !!
+const buttonStyle={
+    margin:80,
+
+}
 
 //MyApp클래스를 다른 .js에서 사용할수 있도록 내보내기
 export default MyApp
